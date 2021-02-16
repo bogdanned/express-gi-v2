73c0238c4d561fa5d7cd6836771f7651f7ce76d1
Bogdan Nedelcu <bogdanned32@gmail.com>
2021-02-16 15:50:20 +0100

    updated tests

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express();
 
+var app = express();
 
 app.get("/", (req,res) => (res.send("hello world!")))
 
 
-app.listen(port, () => (console.log("App running on http://localhost:"+ port)));
+module.exports = app;
